9d1940048402c7d85011bcdc69268dc93a4eda71
Kemoel Amorim Miranda <kemoel.amorim@gmail.com>
2022-03-12 11:02:02 -0300

    removendo importção desnecessária

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -6,7 +6,6 @@ import academy.devdojo.springboot2.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
